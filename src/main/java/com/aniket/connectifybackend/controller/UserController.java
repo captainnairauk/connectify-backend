2edc1df739c3917bed4b14f81e8437575235021f
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2023-12-26 15:56:19 +0530

    Implement spring security

--- src/main/java/com/aniket/connectifybackend/controller/UserController.java
+++ src/main/java/com/aniket/connectifybackend/controller/UserController.java
@@ -22,31 +22,31 @@ public class UserController {
         User savedUser = userService.registerUser(user);
         return savedUser;
     }
-    @GetMapping("/users")
+    @GetMapping("/api/users")
     public List<User> getUsers(){
         List<User> users = userRepository.findAll();
         return users;
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/api/users/{userId}")
     public User getUsersById(@PathVariable("userId") Integer id) throws Exception{
         User user = userService.findUserById(id);
         return user;
     }
 
-    @PutMapping("/users/{userId}")
+    @PutMapping("/api/users/{userId}")
     public User updateUser(@RequestBody User user, @PathVariable Integer userId) throws Exception {
         User updatedUser = userService.updateUser(user, userId);
         return updatedUser;
     }
 
-    @PutMapping("/users/follow/{userId1}/{userId2}")
+    @PutMapping("/api/users/follow/{userId1}/{userId2}")
     public User followUserHandler(@PathVariable Integer userId1, @PathVariable Integer userId2) throws Exception {
         User user = userService.followUser(userId1, userId2);
         return user;
     }
 
-    @GetMapping("/users/search")
+    @GetMapping("/api/users/search")
     public List<User> searchUser(@RequestParam("query") String query){
         List<User>users = userService.searchUser(query);
         return users;
